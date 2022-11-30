a070430f93434377967650259f531faea8f78646
wellitonscheer <wellitonscheer@gmail.com>
2022-11-29 23:00:55 -0300

    ssh test

--- aplicativo_esportes_cti4_app/lib/rpg/Atributos.dart
+++ aplicativo_esportes_cti4_app/lib/rpg/Atributos.dart
@@ -15,6 +15,7 @@ class _AtributosState extends State<Atributos> {
   String titulo = "a";
   int _valor = 0;
   TextEditingController edit = TextEditingController(text: "0");
+  TextEditingController modificador = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,27 +40,52 @@ class _AtributosState extends State<Atributos> {
             ),
           ),
           Expanded(
-            child: TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
-                labelText: widget.texto,
+            child: Container(
+              child: Column(
+                children: [
+                  TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
+                      labelText: widget.texto,
+                    ),
+                    controller: edit,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  SizedBox(
+                    width: 50,
+                    height: 35,
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
+                        labelText: widget.texto,
+                      ),
+                      controller: modificador,
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  )
+                ],
               ),
-              controller: edit,
             )
           ),
           Expanded(
-            child: ElevatedButton(
-              style: ButtonStyle(
-                padding: MaterialStateProperty.all(EdgeInsets.all(5)),
-                iconSize: MaterialStateProperty.all(40)
+            child: SizedBox(
+              height: 100,
+              width: 10,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                      //padding: MaterialStateProperty.all(EdgeInsets.all(5)),
+                      iconSize: MaterialStateProperty.all(40),
+
+                  ),
+                  onPressed: (){
+                    _valor++;
+                    edit.text = '$_valor';
+                  },
+                  child: Icon(Icons.expand_less)
               ),
-              onPressed: (){
-                _valor++;
-                edit.text = '$_valor';
-              },
-              child: Icon(Icons.expand_less)
-            ),
+            )
           ),
         ],
       ),
